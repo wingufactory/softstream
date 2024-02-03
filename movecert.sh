0344d375ba2cf040724b6ef0dc8416e82ed6e88f
K6SY <kalidou.harouna.sy@gmail.com>
2024-02-03 21:04:05 +0100

    Boostrap K8S

---
+++ movecert.sh
@@ -0,0 +1,4 @@
+scp /etc/kubernetes/pki/sa.* root@37.60.239.101:/etc/kubernetes/pki/
+scp /etc/kubernetes/pki/ca.* root@37.60.239.101:/etc/kubernetes/pki/
+scp /etc/kubernetes/pki/front-proxy-ca.* root@37.60.239.101:/etc/kubernetes/pki/
+scp /etc/kubernetes/pki/etcd/ca.* root@37.60.239.101:/etc/kubernetes/pki/etcd/
